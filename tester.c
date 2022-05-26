a80f4597567f0ceeb1df021f0d71909008bafc06
bipsmeister <105847236+bipsmeister@users.noreply.github.com>
2022-05-26 09:35:06 +0200

    Update tester.c

--- tester.c
+++ tester.c
@@ -9,7 +9,7 @@ int igrac = 1, i, odabir;
 
 void ploca(char* x)
 {
-	system("cls");
+
 	printf("\n\t\t    Krizic kruzic igra  \n");
 	printf("\n\t\t         |         |        ");
 	printf("\n\t\t     %c   |    %c    |   %c   "
@@ -26,60 +26,76 @@ char pobjedaXO(char* x)
 	{
 		return 'X';
 	}
- elseif (*(x + 4) == 'X' && *(x + 5) == 'X' && *(x + 6) == 'X')
+ else if (*(x + 4) == 'X' && *(x + 5) == 'X' && *(x + 6) == 'X')
 	{
 		return 'X';
 	}
-elseif (*(x + 7) == 'X' && *(x + 8) == 'X' && *(x + 9) == 'X')
+else if (*(x + 7) == 'X' && *(x + 8) == 'X' && *(x + 9) == 'X')
 	{
 		return 'X';
 	}
-elseif (*(x + 0) == 'X' && *(x + 1) == 'X' && *(x + 2) == 'X')
+else if (*(x + 0) == 'X' && *(x + 1) == 'X' && *(x + 2) == 'X')
 	{
 		return 'X';
 	}
-elseif (*(x + 1) == 'X' && *(x + 4) == 'X' && *(x + 7) == 'X')
+else if (*(x + 1) == 'X' && *(x + 4) == 'X' && *(x + 7) == 'X')
 	{
 		return 'X';
 	}
-elseif (*(x + 2) == 'X' && *(x + 5) == 'X' && *(x + 8) == 'X')
+else if (*(x + 2) == 'X' && *(x + 5) == 'X' && *(x + 8) == 'X')
 	{
 		return 'X';
 	}
-elseif (*(x + 3) == 'X' && *(x + 6) == 'X' && *(x + 9) == 'X')
+else if (*(x + 3) == 'X' && *(x + 6) == 'X' && *(x + 9) == 'X')
 	{
 		return 'X';
 	}
-elseif (*(x + 1) == 'X' && *(x + 5) == 'X' && *(x + 9) == 'X')
+else if (*(x + 1) == 'X' && *(x + 5) == 'X' && *(x + 9) == 'X')
 	{
 		return 'X';
 	}
-elseif (*(x + 3) == 'X' && *(x + 5) == 'X' && *(x + 7) == 'X')
+else if (*(x + 3) == 'X' && *(x + 5) == 'X' && *(x + 7) == 'X')
 	{
 		return 'X';
 	}
          
 
          
-if (*(x + 1) == 'X' && *(x + 2) == 'X' && *(x + 3) == 'X')
+if (*(x + 1) == 'O' && *(x + 2) == 'O' && *(x + 3) == 'O')
 	{
 		return 'O';
 	}
-elseif (*(x + 4) == 'X' && *(x + 5) == 'X' && *(x + 6) == 'X')
+else if (*(x + 4) == 'O' && *(x + 5) == 'O' && *(x + 6) == 'O')
 	{
 		return 'O';
 	}        
-elseif (*(x + 7) == 'X' && *(x + 8) == 'X' && *(x + 9) == 'X')
+else if (*(x + 7) == 'O' && *(x + 8) == 'O' && *(x + 9) == 'O')
 	{
 		return 'O';
 	}
-elseif (*(x + 1) == 'X' && *(x + 4) == 'X' && *(x + 7) == 'X')
+else if (*(x + 1) == 'O' && *(x + 4) == 'O' && *(x + 7) == 'O')
 	{
 		return 'O';
 	}
+else if (*(x + 2) == 'O' && *(x + 5) == 'O' && *(x + 8) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 3) == 'O' && *(x + 6) == 'O' && *(x + 9) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 1) == 'O' && *(x + 5) == 'O' && *(x + 9) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 3) == 'O' && *(x + 5) == 'O' && *(x + 7) == 'O')
+	{
+		return 'O';
+	}	       
 
-
-int main() {
+	       
+	       int main() {
   
   
   int polje[10] = { '1', '2', '3', '4', '5', '6', '7', '8', '9', } 
