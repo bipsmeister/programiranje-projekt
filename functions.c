b41b9b128f7c94cbcb9f36d0b0440576c1fd1546
bipsmeister <105847236+bipsmeister@users.noreply.github.com>
2022-06-02 08:47:08 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -12,4 +12,75 @@ void ploca(char* x)
 
 }
 
+char pobjedaXO(char* x)
+  if (*(x + 1) == 'X' && *(x + 2) == 'X' && *(x + 3) == 'X')
+	{
+		return 'X';
+	}
+ else if (*(x + 4) == 'X' && *(x + 5) == 'X' && *(x + 6) == 'X')
+	{
+		return 'X';
+	}
+else if (*(x + 7) == 'X' && *(x + 8) == 'X' && *(x + 9) == 'X')
+	{
+		return 'X';
+	}
+else if (*(x + 0) == 'X' && *(x + 1) == 'X' && *(x + 2) == 'X')
+	{
+		return 'X';
+	}
+else if (*(x + 1) == 'X' && *(x + 4) == 'X' && *(x + 7) == 'X')
+	{
+		return 'X';
+	}
+else if (*(x + 2) == 'X' && *(x + 5) == 'X' && *(x + 8) == 'X')
+	{
+		return 'X';
+	}
+else if (*(x + 3) == 'X' && *(x + 6) == 'X' && *(x + 9) == 'X')
+	{
+		return 'X';
+	}
+else if (*(x + 1) == 'X' && *(x + 5) == 'X' && *(x + 9) == 'X')
+	{
+		return 'X';
+	}
+else if (*(x + 3) == 'X' && *(x + 5) == 'X' && *(x + 7) == 'X')
+	{
+		return 'X';
+	}
          
+
+         
+if (*(x + 1) == 'O' && *(x + 2) == 'O' && *(x + 3) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 4) == 'O' && *(x + 5) == 'O' && *(x + 6) == 'O')
+	{
+		return 'O';
+	}        
+else if (*(x + 7) == 'O' && *(x + 8) == 'O' && *(x + 9) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 1) == 'O' && *(x + 4) == 'O' && *(x + 7) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 2) == 'O' && *(x + 5) == 'O' && *(x + 8) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 3) == 'O' && *(x + 6) == 'O' && *(x + 9) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 1) == 'O' && *(x + 5) == 'O' && *(x + 9) == 'O')
+	{
+		return 'O';
+	}
+else if (*(x + 3) == 'O' && *(x + 5) == 'O' && *(x + 7) == 'O')
+	{
+		return 'O';
+	}	                
