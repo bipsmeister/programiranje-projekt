e604dd5f8c3e496d63f4535f213afdd0b61b873e
bipsmeister <105847236+bipsmeister@users.noreply.github.com>
2022-06-02 08:35:19 +0200

    Update tester.c

--- tester.c
+++ tester.c
@@ -12,11 +12,11 @@ void ploca(char* x)
 
 	printf("\n\t\t    Krizic kruzic igra  \n");
 	printf("\n\t\t         |         |        ");
-	printf("\n\t\t     %c   |    %c    |   %c   "
+	printf("\n\t\t     %c   |    %c    |   %c, a[1], a[2], a[3]   "
 	printf("\n\t\t --------|---------|--------");
-	printf("\n\t\t     %c   |    %c    |   %c   ")
+	printf("\n\t\t     %c   |    %c    |   %c, a[4], a[5], a[6]   ")
 	printf("\n\t\t --------|---------|--------");
-	printf("\n\t\t     %c   |    %c    |   %c   ")
+	printf("\n\t\t     %c   |    %c    |   %c, a[7], a[8], a[9]   ")
 	printf("\n\t\t         |         |        \n");
 
 }
